0bfbb1ee219ada8aa5fe4f2419bd76be009ee821
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-02-27 00:34:03 +0100

    changed http status code to 403

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,16 +10,16 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-@router.post("/login", status_code=status.HTTP_200_OK)
+@router.post("/login", status_code=status.HTTP_200_OK, response_model=schemas.TokenResponse)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid Credentials")
     
     # Here you would normally verify the password
     if not verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid Credentials")
     
     # create a JWT token and return it to the client
     access_token = oauth2.create_access_token(data={"user_id": user.id})
