a0f25a58b6e98c39efb7af9f4bd6d11132e867b7
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-03-01 20:58:04 +0100

    added pagination (limit & skip to the Get post endpoint
    added search option to the get post endpoint

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 
@@ -11,8 +12,8 @@ router = APIRouter(
 )
 
 @router.get("", status_code=status.HTTP_200_OK, response_model=list[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    posts = db.query(models.Post).filter(models.Post.title.icontains(search)).limit(limit).offset(skip).all()
     return posts
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
