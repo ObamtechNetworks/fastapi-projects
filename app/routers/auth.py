d88ed875d8ce8606f38acf7aedbff689c700b26d
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-03-14 22:49:00 +0100

    renamed invalid to Invalid

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -15,11 +15,11 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
     
     # Here you would normally verify the password
     if not verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
     
     # create a JWT token and return it to the client
     access_token = oauth2.create_access_token(data={"user_id": user.id})
